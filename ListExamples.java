94951bfdbd0e0fb472efc592498a7d4b2eb17736
Kelsie A Lloyd <kalloyd@ieng6-202.ucsd.edu>
2024-05-23 19:33:25 -0700

    Bug fixed

--- ListExamples.java
+++ ListExamples.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 interface StringChecker { boolean checkString(String s); }
 
+
 class ListExamples {
 
   // Returns a new list that has all the elements of the input list for which
@@ -41,7 +42,7 @@ class ListExamples {
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
       // change index1 below to index2 to fix test
-      index1 += 1;
+      index2 += 1;
     }
     return result;
   }
